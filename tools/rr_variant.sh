68dbe31fc46e2bdc8c0fcad9f07ae3fa41bdcd57
Aren Clegg <aclegg2011@gmail.com>
2016-12-22 15:18:58 -0800

    Revert "Add descriptions to the build types"
    This reverts commit b4fa11544f62bff8fe09736cfe5ab6106c1efa8a.

--- tools/rr_variant.sh
+++ tools/rr_variant.sh
@@ -3,24 +3,26 @@
 . $ANDROID_BUILD_TOP/vendor/cm/tools/colors
 echo -e ${ylw}"\n\n ▼ Which build type do you want to choose?\n"${txtrst}
 echo -e "";
-echo -e ${blu}" 〉 1- Official "${txtrst}${red}"    ︱ Option for the only official device maintainers"${txtrst}
+echo -e ${cya}" 〉1- Official"${txtrst}
 echo -e "";
-echo -e ${blu}" 〉 2- Unofficial "${txtrst}${red}"  ︱ Option for the unofficial device builders"${txtrst}
+echo -e ${cya}" 〉2- Unofficial"${txtrst}
 echo -e "";
-echo -e ${blu}" 〉 3- Nightly "${txtrst}${red}"     ︱ Option for the only official daily builders"${txtrst}
+echo -e ${cya}" 〉3- Nightly"${txtrst}
 echo -e "";
-echo -e ${blu}" 〉 4- Weekly "${txtrst}${red}"      ︱ Option for the only official weekly builders"${txtrst}
+echo -e ${cya}" 〉4- Weekly"${txtrst}
 echo -e "";
-echo -e ${blu}" 🕑  30/sec Time-out "${txtrst}${red}"︱ Unofficial by default"${txtrst}
+echo -e ${red}" 〉30/sec Time-out (unofficial)"${txtrst}
 echo -e "";
-echo -e ${cya}" ▼ Pick a number"${txtrst}
+echo -e ${blu}" ▼ Pick a number"${txtrst}
 echo -e "";
 $normal
+if [ -z $askvariant ];then
 read -t 30 askvariant || autovariant=1
+fi
 sleep 1
 if [ "$autovariant" == "1" ]
 then
-export RR_BUILDTYPE=unofficial
+RR_BUILDTYPE=unofficial
 $normal
 sleep 1
 fi
@@ -28,7 +30,7 @@ if [ "$askvariant" == "1" ]
 then
 echo -e "";
 echo -e ${blu}" ▪ Building official variant "${txtrst}
-export RR_BUILDTYPE=official
+RR_BUILDTYPE=official
 echo -e "";
 echo -e "";
 $normal
@@ -38,7 +40,7 @@ if [ "$askvariant" == "2" ]
 then
 echo -e "";
 echo -e ${blu}" ▪ Building unofficial variant "${txtrst}
-export RR_BUILDTYPE=unofficial
+RR_BUILDTYPE=unofficial
 echo -e "";
 echo -e "";
 $normal
@@ -48,7 +50,7 @@ if [ "$askvariant" == "3" ]
 then
 echo -e "";
 echo -e ${blu}" ▪ Building nightly variant "${txtrst}
-export RR_BUILDTYPE=nightly
+RR_BUILDTYPE=nightly
 echo -e "";
 echo -e "";
 $normal
@@ -58,7 +60,7 @@ if [ "$askvariant" == "4" ]
 then
 echo -e "";
 echo -e ${blu}" ▪ Building weekly variant "${txtrst}
-export RR_BUILDTYPE=weekly
+RR_BUILDTYPE=weekly
 echo -e "";
 echo -e "";
 $normal
